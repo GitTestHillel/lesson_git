da0dabbd08b725a01710785f7e95c34173c0c0a2
OTIS <otis01990@gmail.com>
2017-11-02 12:50:12 +0200

    slave commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -1,5 +1,11 @@
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        
     }
 }
